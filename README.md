9dd88c87532c7847fe2ce70a846abb8722331fdc
d10sfan <d10sfan@gmail.com>
2020-05-26 16:43:27 -0500

    Initial

---
+++ README.md
@@ -0,0 +1,8 @@
+# action-build-update-packages
+
+GitHub Action to update packages.json
+
+## Building
+
+* Make any changes to the index.js
+* Once done, run the following command `node_modules/@zeit/ncc/dist/ncc/cli.js build index.js` and create a tag
---
+++ index.js
@@ -0,0 +1,57 @@
+const core = require('@actions/core');
+const { context, GitHub } = require('@actions/github');
+const fs = require('fs').promises;
+const path = require('path');
+
+const packagesFileName = "metadata/packages.json";
+
+console.log('Starting.');
+
+async function run() {
+    try {
+        const fileStr = await fs.readFile(packagesFileName, 'utf-8');
+        const packages = JSON.parse(fileStr);
+        
+        const files = JSON.parse(core.getInput('matrix'));
+        const version = core.getInput('version');
+        
+        const newData = {};
+        
+        for(let i = 0; i < files.length; i++) {
+            const fileName = files[i].name;
+            const extension = files[i].extension;
+            const fileNameArr = fileName.split('-');
+            const engineName = fileNameArr[0];
+            const steamid = fileNameArr[1];
+            
+            console.log(`Found ${engineName} for steam-id ${steamid}`);
+            
+            if(packages[steamid]) {
+                if(!newData[steamid]) {
+                    newData[steamid] = packages[steamid];
+                    newData[steamid].download = [];
+                }
+                
+                newData[steamid].download.push({
+                    name: engineName,
+                    url: `https://bintray.com/luxtorpeda-dev/assets/download_file?file_path=`,
+                    file: `${fileName}-${version}${extension}`
+                });
+                
+                console.log(`Updating ${steamid} to ${JSON.stringify(newData[steamid].download)}`);
+            }
+        }
+        
+        for(let steamid of newData) {
+            packages[steamid] = newData[steamid];
+        }
+        
+        const finalPackagesStr = JSON.stringify(packages, null, 4);
+        await fs.writeFile(packagesFileName, finalPackagesStr);
+    }
+    catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+run();
