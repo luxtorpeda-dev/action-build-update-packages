fe3e487447b3a334f22bef72cc2dd4c212bc5e7b
d10sfan <me@d10sfan.com>
2025-11-20 20:44:09 -0600

    Revert "Update index.js"
    This reverts commit 675c6dc5b5b9cbe7fe7d2ba4334a04b8dc3898a1.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { context, GitHub } = require('@actions/github');
 const fs = require('fs').promises;
 const path = require('path');
 
-const packagesFileName = "metadata/packagesrt4.json";
+const packagesFileName = "metadata/packagessniper_v2.json";
 
 console.log('Starting.');
 
